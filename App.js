a90fb17e1420274e829ba3cd396cdb9ba709e988
Samuel Toh <88163220+samtoh98@users.noreply.github.com>
2021-12-15 13:28:42 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,94 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
+const DATA = [
+  {
+    id: 1,
+    title: 'Pastas',
+  },
+  {
+    id: 2,
+    title: 'Steaks',
+  },
+  {
+    id: 3,
+    title: 'Hamburgers',
+  },
+  {
+    id: 4,
+    title: 'Cheese Burgers',
+  },
+  {
+    id: 5,
+    title: 'Burittos',
+  },
+  {
+    id: 6,
+    title: 'Tacos',
+  },
+  {
+    id: 7,
+    title: 'Bulgogi',
+  },
+  {
+    id: 8,
+    title: 'Chicken Rice',
+  },
+  {
+    id: 9,
+    title: 'Tandoori',
+  },
+  {
+    id: 10,
+    title: 'Cheese Naan',
+  },
+];
+
+function Item({ id, title, selected, onSelect }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => onSelect(id)}
+      style={[
+        styles.item,
+        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+      ]}
+    >
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+function HomeScreen () {
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+    id => {
+      const newSelected = new Map(selected);
+      newSelected.set(id, !selected.get(id));
+
+      setSelected(newSelected);
+    },
+    [selected],
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+          <Item
+            id={item.id}
+            title={item.title}
+            selected={!!selected.get(item.id)}
+            onSelect={onSelect}
+          />
+        )}
+        keyExtractor={item => item.id}
+        extraData={selected}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -21,13 +102,31 @@ function SettingsScreen() {
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Menu" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Menu" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
